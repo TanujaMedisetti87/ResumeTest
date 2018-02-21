b9d6a169035e60aaf923d558bd70425541805546
Tanuja <TanujaMedisetti87@users.noreply.github.com>
2018-02-21 17:55:43 +0530

    Update pom.js

--- pom.js
+++ pom.js
@@ -10,7 +10,7 @@ var release = {
       password:'admin123'
     },
     pomDir: 'pom',
-    url: 'http://ec2-54-209-206-172.compute-1.amazonaws.com:8081/nexus/content/repositories/releases',
+    url: 'http://ec2-54-209-206-172.compute-1.amazonaws.com:8081/nexus/content/repositories/nexus-repo/',
     artifact: 'nexus-deployer.zip',
     noproxy: 'http://ec2-54-209-206-172.compute-1.amazonaws.com',
     cwd: ''
