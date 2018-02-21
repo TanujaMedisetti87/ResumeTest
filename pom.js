a9a40a9d7ff1a86c83ab707093d101e775c5eb89
Tanuja <TanujaMedisetti87@users.noreply.github.com>
2018-02-21 17:31:14 +0530

    Update pom.js

--- pom.js
+++ pom.js
@@ -1,21 +1,23 @@
-grunt.initConfig({
-  nexusDeployer: {
-    release: {
-      options: {
-          groupId: "nexus-deployer",
-          artifactId: "nexus-deployer",
-          version: "1.0",
-          packaging: 'zip',http://ec2-54-209-206-172.compute-1.amazonaws.com:8081
-          auth: {
-            username:'admin',
-            password:'admin123'
-          },
-          pomDir: 'pom',
-          url: 'http://ec2-54-209-206-172.compute-1.amazonaws.com:8081/nexus/content/repositories/releases',
-          artifact: 'build/grunt-nexus-deployer.zip',
-          noproxy: 'http://ec2-54-209-206-172.compute-1.amazonaws.com',
-          cwd: ''
-        }
-      }
-    }
+var deployer = require('nexus-deployer');
+ 
+var release = {
+    groupId: 'nexus-deployer',
+    artifactId: 'nexus-deployer',
+    version: '1.0',
+    packaging: 'zip',
+    auth: {
+      username:'admin',
+      password:'admin123'
+    },
+    pomDir: 'pom',
+    url: 'http://ec2-54-209-206-172.compute-1.amazonaws.com:8081/nexus/content/repositories/releases',
+    artifact: 'nexus-deployer.zip',
+    noproxy: 'http://ec2-54-209-206-172.compute-1.amazonaws.com',
+    cwd: ''
+};
+ 
+ 
+deployer.deploy(release, function(){
+    // your async call back here 
+    // done(); 
 });
